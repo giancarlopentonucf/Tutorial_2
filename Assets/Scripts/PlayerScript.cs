c770a4c4e6b35cf55ce6436edf4895bf7effe897
giancarlopentonucf <70389731+giancarlopentonucf@users.noreply.github.com>
2021-03-09 15:28:40 -0500

    Challenge commit 1

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,13 +11,33 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text lives;
+
+    public Text winText;
+
+    public Text livesText;
+
+    public Text loseText;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+
     private int scoreValue = 0;
+
+    private int livesValue = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
     }
 
     // Update is called once per frame
@@ -27,12 +47,24 @@ public class PlayerScript : MonoBehaviour
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
 
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
+        if (livesValue <= 0)
+        {
+            loseText.text = "You Died";
+            Destroy (gameObject);
+
+        }
+        if (scoreValue == 4)
+        {
+            livesValue = 3;
+            lives.text = livesValue.ToString();
+        }
     }
-        
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -42,7 +74,27 @@ public class PlayerScript : MonoBehaviour
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
+        if (scoreValue >= 8)
+        {
+            winText.text = "You Win! Birthed by Giancarlo Penton";
+            musicSource.clip = musicClipOne;
+            musicSource.Stop();
 
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+
+        }
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector3(50.0f, 0.0f, 0.0f);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
